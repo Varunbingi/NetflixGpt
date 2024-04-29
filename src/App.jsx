35634c9f7067bb9331b774d64498170145a57dae
Varunbingi <bingivarun27@gmail.com>
2024-04-29 12:06:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-5xl font-semibold text-rose-800'>Netflix Gpt</h1>
+    </>
+  )
+}
+
+export default App
